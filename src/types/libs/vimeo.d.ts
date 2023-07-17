8e94b6ef3f87f7185cf15da5cab479073e5b3d17
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-07-18 03:12:06 +0400

    types: update vimeo.

--- src/types/libs/vimeo.d.ts
+++ src/types/libs/vimeo.d.ts
@@ -1,17 +1,1 @@
 declare module '@vimeo/vimeo'
-
-type UploadFileParams = {
-  filePath: string,
-  name: string,
-  description: string
-}
-
-type UpdateFileDescriptionParams = {
-  uri: string,
-  name: string,
-  description: string
-}
-
-type UpdateDescriptionResponse = {
-  statusCode: number
-}
