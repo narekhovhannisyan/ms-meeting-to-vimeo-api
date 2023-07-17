741ff5688e6e2c9878a0a19765f3a4fa3562ed2e
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-07-18 03:51:43 +0400

    services: add logging.

--- src/api/v1/services/meeting-transfer.service.ts
+++ src/api/v1/services/meeting-transfer.service.ts
@@ -12,31 +12,41 @@ import { handleGet } from '../../../utils/response-handler.util'
 export const executeStrategy = async (request: Request, response: Response, next: NextFunction) => {
   try {
     const meetings = await CalendarioModel.getMeetings()
+    console.log('Meetings fetched.')
 
     for (const meeting of meetings) {
       const meetingId = meeting.meetingId
       const ifExist = await RepositorioModel.getRepoByMeetingId(meetingId)
 
       if (!ifExist) {
+        console.log('Repository check passed.')
         const repository = await RepositorioModel.insertRepoByMeetingId(meetingId)
         const recordings = await getAllCallRecordings()
-        const { callRecordingUrl, callRecordingDisplayName } = recordings?.find((recording) => recording.callId === meetingId)
+        console.log('Got call recordings.')
+        const callRecordingData = recordings?.find((recording) => recording.callId === meetingId)
 
-        const filePath = `${path}/downloads/${callRecordingDisplayName}`
+        if (callRecordingData) {
+          console.log('Download file flow.')
+          const { callRecordingUrl, callRecordingDisplayName } = callRecordingData
 
-        await downloadFile(callRecordingUrl, filePath)
+          const filePath = `${path}/downloads/${callRecordingDisplayName}`
 
-        const urlVideo = await uploadVideo({
-          filePath,
-          name: callRecordingDisplayName,
-          description: callRecordingDisplayName
-        })
+          await downloadFile(callRecordingUrl, filePath)
 
-        await TemaSubtemaModel.crateTemaRecord({
-          urlVideo,
-          nombre: callRecordingDisplayName,
-          idRepositorio: repository.idRepositorio
-        })
+          console.log('Uploading file.')
+          const urlVideo = await uploadVideo({
+            filePath,
+            name: callRecordingDisplayName,
+            description: callRecordingDisplayName
+          })
+
+          console.log('Uploaded file.')
+          await TemaSubtemaModel.crateTemaRecord({
+            urlVideo,
+            nombre: callRecordingDisplayName,
+            idRepositorio: repository.idRepositorio
+          })
+        }
       }
     }
 
