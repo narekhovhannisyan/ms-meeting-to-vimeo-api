db9d6a4adf1769367276a642ba8745b945bf7845
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-07-18 03:05:46 +0400

    service: implement meeting transfer.

--- src/api/v1/services/meeting-transfer.service.ts
+++ src/api/v1/services/meeting-transfer.service.ts
@@ -1,6 +1,12 @@
 import { NextFunction, Request, Response } from 'express'
+import { path } from 'app-root-path'
 
-import { CalendarioModel, RepositorioModel } from '../../../models'
+import { CalendarioModel, RepositorioModel, TemaSubtemaModel } from '../../../models'
+
+import { getAllCallRecordings } from '../../../libs/microsoft-graph-api.lib'
+import { downloadFile } from '../../../libs/download-file.lib'
+import { uploadVideo } from '../../../libs/vimeo.lib'
+
 import { handleGet } from '../../../utils/response-handler.util'
 
 export const executeStrategy = async (request: Request, response: Response, next: NextFunction) => {
@@ -9,10 +15,28 @@ export const executeStrategy = async (request: Request, response: Response, next
 
     for (const meeting of meetings) {
       const meetingId = meeting.getDataValue('meetingid')
-      const ifExist = await RepositorioModel.insertRepoByMeetingId(meetingId)
+      const ifExist = await RepositorioModel.getRepoByMeetingId(meetingId)
 
       if (!ifExist) {
+        const repository = await RepositorioModel.insertRepoByMeetingId(meetingId)
+        const recordings = await getAllCallRecordings()
+        const { callRecordingUrl, callRecordingDisplayName } = recordings?.find((recording) => recording.callId === meetingId)
 
+        const filePath = `${path}/downloads/${callRecordingDisplayName}`
+
+        await downloadFile(callRecordingUrl, filePath)
+
+        const urlVideo = await uploadVideo({
+          filePath,
+          name: callRecordingDisplayName,
+          description: callRecordingDisplayName
+        })
+
+        await TemaSubtemaModel.crateTemaRecord({
+          urlVideo,
+          nombre: callRecordingDisplayName,
+          idRepositorio: repository.idRepositorio
+        })
       }
     }
 
