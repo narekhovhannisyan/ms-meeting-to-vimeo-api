5ec3eae5b228a35d2dc1d3ce4fdc3e50b5411ad2
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-07-18 03:05:34 +0400

    models: add methods to repo, tema.

--- src/models/repositorio.model.ts
+++ src/models/repositorio.model.ts
@@ -1,11 +1,25 @@
-import { FindOrCreateOptions } from 'sequelize'
+import { FindOptions, FindOrCreateOptions } from 'sequelize'
 
 import { RepositorioDefinition } from './definitions'
 
 /**
+ * Gets repositorio by meeting id.
+ */
+export const getRepoByMeetingId = (meetingId: string) => {
+  const withCriteria: FindOptions = {
+    where: {
+      clave: meetingId
+    },
+    raw: true
+  }
+
+  return RepositorioDefinition.findOne(withCriteria)
+}
+
+/**
  * Inserts repository by given `meetingId`.
  */
-export const insertRepoByMeetingId = (meetingId: string) => {
+export const insertRepoByMeetingId = (meetingId: string): Promise<any> => {
   const withCriteria: FindOrCreateOptions = {
     where: {
       nombre: meetingId,
