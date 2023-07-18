bb4a5ac05a29064788125b6a0ab31f14e62e77ee
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-07-18 16:24:31 +0400

    libs: tune graph api to provide meeting code.

--- src/libs/microsoft-graph-api.lib.ts
+++ src/libs/microsoft-graph-api.lib.ts
@@ -34,16 +34,32 @@ const getUserIds = async () => {
 }
 
 /**
- * Gets chats for given `userId`.
+ * Gets chats for given `userId` and filters meeting ones.
  */
 const getUserChats = async (userId: string) => {
   try {
     const userChats = await graphClient.api(`/users/${userId}/chats`).get()
 
-    return userChats.value
+    return userChats.value.filter((chat) => chat.chatType === 'meeting').map((chat) => {
+      chat.userId = userId
+
+      return chat
+    })
   } catch (error) {
     console.error(error)
   }
+}
+
+/**
+ * Builds `meetingId` from `joinWebUrl` string.
+ */
+const convertMessageIdToMeetingId = (joinWebUrl: string) => {
+  const decodedJoinWebURL = decodeURIComponent(joinWebUrl)
+  const meetingId = decodedJoinWebURL.split('/')[5]
+  const organizerId = JSON.parse(decodedJoinWebURL.split(`context=`)[1].replaceAll('”', '"').replaceAll(`'`, '').replaceAll('″', '"').replaceAll('“', '"')).Oid
+  const meetindId = `1*${organizerId}*0**${meetingId}`
+
+  return Buffer.from(meetindId).toString('base64')
 }
 
 /**
@@ -53,7 +69,12 @@ const getChatMessages = async (chat: any) => {
   try {
     const messages = await graphClient.api(`/chats/${chat.id}/messages`).get()
 
-    return messages.value
+    return messages.value.map((message) => {
+      message.meetingId = convertMessageIdToMeetingId(chat.onlineMeetingInfo.joinWebUrl)
+      message.userId = chat.userId
+
+      return message
+    })
   } catch (error) {
     return []
   }
@@ -66,6 +87,9 @@ const filterChatMessagesByEvent = (chats: any) => {
   const filteredChatsByEventDetail = chats.map((messagesPerChat: any) => {
     const filteredMessages = messagesPerChat.filter((message: any) => {
       if (message.eventDetail && message.eventDetail.callRecordingUrl) {
+        message.eventDetail.meetingId = message.meetingId
+        message.eventDetail.userId = message.userId
+
         return message.eventDetail
       }
     })
@@ -77,21 +101,51 @@ const filterChatMessagesByEvent = (chats: any) => {
 }
 
 /**
- * Get all call recordings URLs.
+ * Gets meeting code by `userId` and `meetingId` in base 64 encoding.
+ */
+const getMeetingCodeByMeetingId = async (userId: string, meetingIdBase64: string) => {
+  try {
+    const meetingData = await graphClient.api(`/users/${userId}/onlineMeetings/${meetingIdBase64}`).get()
+
+    return meetingData.value.meetingCode
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+/**
+ * Appends meeting code to given call recording object.
+ */
+const appendMeetingCode = async (callRecording: any) => {
+  const { userId, meetingId } = callRecording
+
+  callRecording.meetingCode = await getMeetingCodeByMeetingId(userId, meetingId)
+
+  return callRecording
+}
+
+/**
+ * 1. Gets all user ids from organization.
+ * 2. Fetches chats for all users.
+ * 3. Fetches messages for all chats.
+ * 4. Gathers call recording objects and flattens array to be one dimensional.
+ * 5. Appends meeting code to each object.
  */
 export const getAllCallRecordings = async () => {
+  const concurrency = { concurrency: 2 }
   try {
     const userIds = await getUserIds()
-    const chatsForEachUser = await Bluebird.map(userIds, getUserChats, { concurrency: 2 })
-
+    const chatsForEachUser = await Bluebird.map(userIds, getUserChats, concurrency)
     const messagesForAllUsers = await Bluebird.map(chatsForEachUser, async (chatsForSingleUser) => {
-      const messagesForSingleUser = await Bluebird.map(chatsForSingleUser, getChatMessages, { concurrency: 2 })
+      const messagesForSingleUser = await Bluebird.map(chatsForSingleUser, getChatMessages, concurrency)
       const calls = filterChatMessagesByEvent(messagesForSingleUser)
 
       return calls
-    }, { concurrency: 2 })
+    }, concurrency)
 
-    return messagesForAllUsers.flat(2)
+    const allCallRecordings = messagesForAllUsers.flat(2) // flatten array to be one dimensional
+
+    return Bluebird.map(allCallRecordings, appendMeetingCode, concurrency)
   } catch (error) {
     console.error(error)
   }
